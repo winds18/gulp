6b331872b7b341e0f43de8f02f077138a6521990
winds18 <wbwinds18@gmail.com>
2016-07-05 13:23:43 +0800

    for cnnb only

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('browser-sync',function(){
 });
 
 gulp.task('style',function(){
-    return gulp.src('src/styles/**/*.scss')
+    return gulp.src('src/styles/**/main.scss')
         .pipe(sass({style:'expanded'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('pub/css'))
